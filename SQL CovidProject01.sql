a6fe96ad19ee4b3b98bc26655aacdbd9c3c8c7b9
Ahmed Gamal (Jimmy) <109757205+ahmedgamalX55@users.noreply.github.com>
2023-02-01 15:50:49 +0200

    Update SQL CovidProject01.sql

--- SQL CovidProject01.sql
+++ SQL CovidProject01.sql
@@ -1,4 +1,4 @@
---Looking at Total_death Vs Total_cases 
+--Total_death Vs Total_cases 
 
 Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From ProtoProject1.dbo.CovidDeaths as pcd
@@ -7,7 +7,7 @@ Order by 1,2 DESC
 
 
 
---Looking at Total_cases Vs Population
+--Total_cases Vs Population
 
 Select location, date, total_cases, population, (total_cases/population)*100 as CasesPercentage
 From ProtoProject1.dbo.CovidDeaths as pcd
@@ -15,7 +15,7 @@ From ProtoProject1.dbo.CovidDeaths as pcd
 Order by 1,2 
 
 
---Looking at the Countries with highest inflection rate compare to population
+--Countries with highest inflection rate vs population
 
 Select location, Max(total_cases)as Max_Inflected, population, Max((total_cases/population))*100 as Max_Cases_Percent
 From ProtoProject1.dbo.CovidDeaths as pcd
@@ -23,7 +23,7 @@ Group by location,population
 Order by  Max_Cases_Percent DESC
 
 
---Looking at Highest Total death per population
+--Highest Total death per population
 
 Select location, Max(cast(total_deaths as int))as Max_Totaldeath
 From ProtoProject1.dbo.CovidDeaths as pcd
@@ -33,7 +33,7 @@ Order by Max_Totaldeath DESC
 
 
 
---Looking at Highest Total death per Continent
+--Highest Total death per Continent
 
 Select continent, Max(cast(total_deaths as int))as Max_Totaldeath
 From ProtoProject1.dbo.CovidDeaths as pcd
@@ -43,7 +43,7 @@ Order by Max_Totaldeath DESC
 
 
 
---Looking for Globlal numbers of casses and deathes
+--Globlal numbers of casses and deathes
 
 Select date, Sum(new_cases) as TotalCases, Sum(cast(new_deaths as float)) as TotalDeaths, 
 Sum(cast(new_deaths as float))/Sum(new_cases)*100 as GlobalDeaths_perctage_perCases
@@ -62,7 +62,7 @@ Where continent Is not Null
 
 
 
--- Looking at Total population vs Vaccintations 
+--Total population vs Vaccintations 
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations ,
 Sum(cast(vac.new_vaccinations as int)) Over(partition by dea.location order by dea.location , dea.date) as Total_vac 
@@ -73,7 +73,7 @@ Order by 2,3
 
 
 
---CTE POP VS VAC
+--POP VS VAC
 
 With PopvsVac (continent, location, date, population, new_vaccinations ,Total_vac)
 as (
@@ -88,7 +88,7 @@ Select *, (Total_vac/population)*100 as Total_vacpercentage
 from PopvsVac
 
 
---Temp Table
+--Table001
 Drop Table if exists #Total_Pop_VacPercentage
 Create table #Total_Pop_VacPercentage
 (continent nvarchar(255), location nvarchar(255), date datetime,  population numeric, 
@@ -105,7 +105,7 @@ Select *, (Total_vac/population)*100 as Total_vacpercentage
 from #Total_Pop_VacPercentage
 
 
---Create View for Visualization later on 
+--View for Visualization
 Create View Total_Pop_VacPercentage as 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations ,
 Sum(cast(vac.new_vaccinations as int)) Over(partition by dea.location order by dea.location,dea.date) as Total_vac 
